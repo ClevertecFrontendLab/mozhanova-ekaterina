92777b08deea88a65aa9f95eb93725eccda58c86
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-04-02 12:10:20 +0300

    feat(router): config basic routing

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from 'react-router';
+
+import { Router } from './app/router/Router';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+    );
+}
+
+export default App;
+
+//Сверстаны: Главная страница, страница Веганская кухня, страница Самое сочное,
---
+++ src/app/router/Router.tsx
@@ -0,0 +1,33 @@
+import { Route, Routes } from 'react-router';
+
+import { MainLayout } from '~/components/layouts/MainLayout/MainLayout';
+import { Home } from '~/pages/HomePage';
+import { TheJuiciest } from '~/pages/TheJuiciestPage';
+import { VeganCuisine } from '~/pages/VeganCuisinePage';
+
+export const Router = () => (
+    <Routes>
+        <Route element={<MainLayout />}>
+            <Route index path='/' element={<Home />} />
+            <Route path='/vegan-cuisine' element={<VeganCuisine />} />
+            <Route path='/the-juiciest' element={<TheJuiciest />} />
+        </Route>
+    </Routes>
+);
+
+{
+    /* <Routes>
+    <Route element={<MarketingLayout />}>
+        <Route index element={<MarketingHome />} />
+        <Route path='contact' element={<Contact />} />
+    </Route>
+
+    <Route path='projects'>
+        <Route index element={<ProjectsHome />} />
+        <Route element={<ProjectsLayout />}>
+            <Route path=':pid' element={<Project />} />
+            <Route path=':pid/edit' element={<EditProject />} />
+        </Route>
+    </Route>
+</Routes>; */
+}
