5741aba38dd3a8735356e31a5dfbb05badc6ba60
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-04-05 13:53:06 +0300

    feat(css): add chakra provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router';
 
 import { Router } from './app/router/Router';
+import theme from './theme';
 
 function App() {
     return (
         <BrowserRouter>
-            <Router />
+            <ChakraProvider theme={theme}>
+                <Router />
+            </ChakraProvider>
         </BrowserRouter>
     );
 }
