3c039996aaa5d93805109c557281646e79dcfcbb
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-04-06 14:50:12 +0300

    refactor(constants): create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,60 @@
+export const categories = [
+    {
+        title: 'Салаты',
+        iconSrc: '/src/assets/icons/menu_icon_1.png',
+    },
+    {
+        title: 'Закуски',
+        iconSrc: '/src/assets/icons/menu_icon_2.png',
+    },
+    {
+        title: 'Первые блюда',
+        iconSrc: '/src/assets/icons/menu_icon_3.png',
+    },
+    {
+        title: 'Вторые блюда',
+        iconSrc: '/src/assets/icons/menu_icon_4.png',
+    },
+    {
+        title: 'Десерты, выпечка',
+        iconSrc: '/src/assets/icons/menu_icon_5.png',
+    },
+    {
+        title: 'Блюда на гриле',
+        iconSrc: '/src/assets/icons/menu_icon_6.png',
+    },
+    {
+        title: 'Веганская кухня',
+        iconSrc: '/src/assets/icons/menu_icon_7.png',
+        children: [
+            'Закуски',
+            'Первые блюда',
+            'Вторые блюда',
+            'Гарниры',
+            'Десерты',
+            'Выпечка',
+            'Сыроедческие блюда',
+            'Напитки',
+        ],
+    },
+    {
+        title: 'Детские блюда',
+        iconSrc: '/src/assets/icons/menu_icon_8.png',
+    },
+    {
+        title: 'Лечебное питание',
+        iconSrc: '/src/assets/icons/menu_icon_9.png',
+    },
+    {
+        title: 'Национальные',
+        iconSrc: '/src/assets/icons/menu_icon_10.png',
+    },
+    {
+        title: 'Соусы',
+        iconSrc: '/src/assets/icons/menu_icon_11.png',
+    },
+    {
+        title: 'Напитки',
+        iconSrc: '/src/assets/icons/menu_icon_12.png',
+    },
+];
