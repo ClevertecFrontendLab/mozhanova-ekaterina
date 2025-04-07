57b6bb7c8919801271c45493bba80f80349cf400
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-04-07 13:00:37 +0300

    refactor(ui card): add data prop, make appropriate changes

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,52 +1,34 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Grid } from '@chakra-ui/react';
 
-import { categories } from '~/constants';
+import { TRecipe } from '~/types';
 
 import { SectionHeading } from './SectionHeading';
 import { UiCard } from './ui/UiCard';
 import { UiSliderButton } from './ui/UiSliderButton';
 
-export function Slider() {
+type Props = {
+    data: TRecipe[];
+};
+
+export function Slider({ data }: Props) {
     return (
         <Flex direction='column' gap='24px'>
             <SectionHeading title='Новые рецепты' />
-            <Flex gap='24px' position='relative'>
-                <UiCard
-                    imgSrc='/src/assets/slider_1.png'
-                    title='Солянка с грибами'
-                    text='Как раз после праздников, когда мясные продукты еще остались, но никто их уже не хочет, время варить солянку.'
-                    category={categories[2]}
-                    favorites={0}
-                    likes={1}
-                />
-                <UiCard
-                    imgSrc='/src/assets/slider_2.png'
-                    title='Капустные котлеты'
-                    text='Как раз после праздников, когда мясные продукты еще остались, но никто их уже не хочет, время варить солянку.'
-                    category={categories[3]}
-                    favorites={2}
-                    likes={1}
-                />
-                <UiCard
-                    imgSrc='/src/assets/slider_3.png'
-                    title='Оладьи на кефире "Пышные"'
-                    text='Как раз после праздников, когда мясные продукты еще остались, но никто их уже не хочет, время варить солянку.'
-                    category={categories[3]}
-                    favorites={0}
-                    likes={1}
-                />
-                <UiCard
-                    imgSrc='/src/assets/slider_4.png'
-                    title='Салат "Здоровье"'
-                    text='Как раз после праздников, когда мясные продукты еще остались, но никто их уже не хочет, время варить солянку.'
-                    category={categories[3]}
-                    favorites={0}
-                    likes={0}
-                />
-
+            <Grid position='relative' templateColumns='repeat(4, 1fr)' gap='24px'>
+                {data.map((recipe) => (
+                    <UiCard
+                        key={recipe.id}
+                        imgSrc={recipe.imageSrc}
+                        title={recipe.title}
+                        text={recipe.description}
+                        category={recipe.category}
+                        favorites={recipe.favorites}
+                        likes={recipe.likes}
+                    />
+                ))}
                 <UiSliderButton direction='left' />
                 <UiSliderButton direction='right' />
-            </Flex>
+            </Grid>
         </Flex>
     );
 }
