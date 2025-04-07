b60be74cbe97b83e7197b2130aa75946f60bf733
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-04-07 13:07:05 +0300

    feat(page): init juiciest page

---
+++ src/pages/TheJuiciestPage.tsx
@@ -0,0 +1,9 @@
+import { PageToolbar } from '~/components/PageToolbar';
+
+export function TheJuiciest() {
+    return (
+        <>
+            <PageToolbar title='Самое сочное' />
+        </>
+    );
+}
