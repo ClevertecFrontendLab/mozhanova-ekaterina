30531154c4d28440aaa5fc3031b4e21f9575150b
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-04-07 14:17:19 +0300

    feat(juiciest page): add layout, cards block, relevant kitchen block

--- src/pages/TheJuiciestPage.tsx
+++ src/pages/TheJuiciestPage.tsx
@@ -1,9 +1,47 @@
+import { Box, Flex, Grid } from '@chakra-ui/react';
+
 import { PageToolbar } from '~/components/PageToolbar';
+import { RelevantKitchenBlock } from '~/components/RelevantKitchenBlock';
+import { UiButton } from '~/components/ui/UiButton';
+import { UiCard } from '~/components/ui/UiCard';
+import { data_relevant_vegan, data_vegan } from '~/constants';
 
 export function TheJuiciest() {
     return (
-        <>
-            <PageToolbar title='Самое сочное' />
-        </>
+        <div>
+            <Box>
+                <PageToolbar title='Самое сочное' />
+            </Box>
+            <Grid
+                templateColumns='repeat(2, 1fr)'
+                templateRows='repeat(4, 1fr)'
+                rowGap='16px'
+                columnGap='24px'
+            >
+                {data_vegan.map((recipe) => (
+                    <UiCard
+                        key={recipe.id}
+                        title={recipe.title}
+                        text={recipe.description}
+                        imgSrc={recipe.imageSrc}
+                        category={recipe.category}
+                        likes={recipe.likes}
+                        favorites={recipe.favorites}
+                        direction='row'
+                        infoPosition='top'
+                        controls
+                        categoryBgColor='secondary.100'
+                    />
+                ))}
+            </Grid>
+            <Flex justifyContent='center' mt='16px' mb='40px'>
+                <UiButton size='md' text='Загрузить еще' variant='primary' />
+            </Flex>
+            <RelevantKitchenBlock
+                data={data_relevant_vegan}
+                heading='Веганская кухня'
+                description='Интересны не только убеждённым вегетарианцам, но и тем, кто хочет  попробовать вегетарианскую диету и готовить вкусные  вегетарианские блюда.'
+            />
+        </div>
     );
 }
