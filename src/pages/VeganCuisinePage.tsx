54fe5b2568bcfd0c5d3a3a442fed6c1490bad9d3
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-04-07 14:18:07 +0300

    feat(vegan page): add relevant kitchen block, load more button

--- src/pages/VeganCuisinePage.tsx
+++ src/pages/VeganCuisinePage.tsx
@@ -1,14 +1,17 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 
 import { PageTabs } from '~/components/PageTabs';
 import { PageToolbar } from '~/components/PageToolbar';
+import { RelevantKitchenBlock } from '~/components/RelevantKitchenBlock';
+import { UiButton } from '~/components/ui/UiButton';
+import { data_relevant_desert } from '~/constants';
 
 export function VeganCuisine() {
     return (
-        <Box>
+        <div>
             <Box pb='24px'>
                 <PageToolbar
-                    title='Приятного аппетита!'
+                    title='Веганская кухня'
                     description='Интересны не только убеждённым вегетарианцам, но и тем, кто хочет  попробовать вегетарианскую диету и готовить вкусные  вегетарианские блюда.'
                 />
             </Box>
@@ -25,6 +28,14 @@ export function VeganCuisine() {
                     'Напитки',
                 ]}
             />
-        </Box>
+            <Flex justifyContent='center' mt='16px' mb='40px'>
+                <UiButton size='md' text='Загрузить еще' variant='primary' />
+            </Flex>
+            <RelevantKitchenBlock
+                data={data_relevant_desert}
+                heading='Десерты, выпечка'
+                description='Без них невозможно представить себе ни современную, ни традиционную  кулинарию. Пироги и печенья, блины, пончики, вареники и, конечно, хлеб - рецепты изделий из теста многообразны и невероятно популярны.'
+            />
+        </div>
     );
 }
