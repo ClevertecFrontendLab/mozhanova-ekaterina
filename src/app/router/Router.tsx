52f36f7aaac9dfbc970355794104849b44cf8566
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-04-12 15:25:22 +0300

    feat(route): add dynamic route

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router';
 
 import { MainLayout } from '~/components/layouts/MainLayout';
+import { DishesByType } from '~/pages/DishesByType ';
 import { Home } from '~/pages/HomePage';
 import { TheJuiciest } from '~/pages/TheJuiciestPage';
 import { VeganCuisine } from '~/pages/VeganCuisinePage';
@@ -9,25 +10,10 @@ export const Router = () => (
     <Routes>
         <Route element={<MainLayout />}>
             <Route index path='/' element={<Home />} />
-            <Route path='/vegan-cuisine' element={<VeganCuisine />} />
+            <Route path='/vegan-cuisine' element={<VeganCuisine />}>
+                <Route path=':categoryId' element={<DishesByType />} />
+            </Route>
             <Route path='/the-juiciest' element={<TheJuiciest />} />
         </Route>
     </Routes>
 );
-
-{
-    /* <Routes>
-    <Route element={<MarketingLayout />}>
-        <Route index element={<MarketingHome />} />
-        <Route path='contact' element={<Contact />} />
-    </Route>
-
-    <Route path='projects'>
-        <Route index element={<ProjectsHome />} />
-        <Route element={<ProjectsLayout />}>
-            <Route path=':pid' element={<Project />} />
-            <Route path=':pid/edit' element={<EditProject />} />
-        </Route>
-    </Route>
-</Routes>; */
-}
