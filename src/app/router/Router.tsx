6e2f970bbb2e1b184b587303d12b58a49af37713
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-04-17 14:26:43 +0300

    refactor(router): add routes

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -1,18 +1,21 @@
 import { Route, Routes } from 'react-router';
 
 import { MainLayout } from '~/components/layouts/MainLayout';
+import { PageTabs } from '~/components/PageTabs';
+import { categories } from '~/mocks/categories';
 import { DishesByType } from '~/pages/DishesByType';
 import { Home } from '~/pages/HomePage';
 import { TheJuiciest } from '~/pages/TheJuiciestPage';
-import { VeganCuisine } from '~/pages/VeganCuisinePage';
 
 export const Router = () => (
     <Routes>
         <Route element={<MainLayout />}>
             <Route index path='/' element={<Home />} />
-            <Route path='/vegan-cuisine' element={<VeganCuisine />}>
-                <Route path=':categoryId' element={<DishesByType />} />
-            </Route>
+            {categories.map((category) => (
+                <Route key={category.id} path={`/${category.id}/*`} element={<DishesByType />}>
+                    <Route path=':subCategoryId' element={<PageTabs />} />
+                </Route>
+            ))}
             <Route path='/the-juiciest' element={<TheJuiciest />} />
         </Route>
     </Routes>
--- src/pages/DishesByType.tsx
+++ src/pages/DishesByType.tsx
@@ -1,9 +1,20 @@
-import { PageTabs } from '~/components/PageTabs';
+import { Outlet } from 'react-router';
+
+import { PageToolbar } from '~/components/PageToolbar';
 
 export function DishesByType() {
     return (
         <div>
-            <PageTabs />
+            <PageToolbar
+                title='Веганская кухня'
+                description='Интересны не только убеждённым вегетарианцам, но и тем, кто хочет  попробовать вегетарианскую диету и готовить вкусные  вегетарианские блюда.'
+            />
+            <Outlet />
+            {/* <RelevantKitchenBlock
+                data={data_relevant_desert}
+                heading='Десерты, выпечка'
+                description='Без них невозможно представить себе ни современную, ни традиционную  кулинарию. Пироги и печенья, блины, пончики, вареники и, конечно, хлеб - рецепты изделий из теста многообразны и невероятно популярны.'
+            /> */}
         </div>
     );
 }
