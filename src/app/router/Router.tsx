df5304d0439eb34c9a6a0f4280fefb7449f0de07
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-04-19 19:19:58 +0300

    feat(router): add recipe page path

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -1,21 +1,23 @@
 import { Route, Routes } from 'react-router';
 
 import { MainLayout } from '~/components/layouts/MainLayout';
-import { PageTabs } from '~/components/shared/PageTabs';
-import { categories } from '~/mocks/categories';
+import { PageTabs } from '~/components/PageTabs';
 import { DishesByType } from '~/pages/DishesByType';
 import { Home } from '~/pages/HomePage';
+import { RecipePage } from '~/pages/ResipePage';
 import { TheJuiciest } from '~/pages/TheJuiciestPage';
 
 export const Router = () => (
     <Routes>
         <Route element={<MainLayout />}>
             <Route index path='/' element={<Home />} />
-            {categories.map((category) => (
-                <Route key={category.id} path={`/${category.id}/*`} element={<DishesByType />}>
-                    <Route path=':subCategoryId' element={<PageTabs />} />
-                </Route>
-            ))}
+
+            <Route path='/:category/*' element={<DishesByType />} errorElement=''>
+                <Route path=':subCategory' element={<PageTabs />} />
+            </Route>
+
+            <Route path='/:category/:subCategory/:id' element={<RecipePage />} errorElement='' />
+
             <Route path='/the-juiciest' element={<TheJuiciest />} />
         </Route>
     </Routes>
