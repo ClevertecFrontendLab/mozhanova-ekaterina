262c86a806c4fb81c57bc7a906585f79a565abc9
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-05-07 01:47:23 +0300

    feat(loader): add global loader to main layout

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router';
 
 import { FooterMobile } from '../footer/FooterMobile';
+import { GlobalLoader } from '../GlobalLoader';
 import { Header } from '../header/Header';
 import { Navbar } from '../Navbar';
 import { Sidebar } from '../Sidebar';
@@ -14,46 +15,49 @@ export function MainLayout() {
     useEffect(() => setMenuOpen(isLargerThanMD), [isLargerThanMD]);
 
     return (
-        <Box
-            pt={{
-                base: '64px',
-                md: '80px',
-            }}
-            pb={{
-                base: '84px',
-                md: '0',
-            }}
-            pl={{
-                base: '0',
-                md: '256px',
-            }}
-            pr={{
-                base: '0',
-                md: '256px',
-            }}
-        >
-            <Header isMenuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <>
+            <GlobalLoader />
+            <Box
+                pt={{
+                    base: '64px',
+                    md: '80px',
+                }}
+                pb={{
+                    base: '84px',
+                    md: '0',
+                }}
+                pl={{
+                    base: '0',
+                    md: '256px',
+                }}
+                pr={{
+                    base: '0',
+                    md: '256px',
+                }}
+            >
+                <Header isMenuOpen={menuOpen} setMenuOpen={setMenuOpen} />
 
-            <Navbar isMenuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+                <Navbar isMenuOpen={menuOpen} setMenuOpen={setMenuOpen} />
 
-            <main>
-                <Outlet />
-            </main>
-            {isLargerThanMD ? (
-                <Box
-                    bg='background.base'
-                    position='fixed'
-                    right='0'
-                    bottom='0'
-                    top='80px'
-                    w='256px'
-                    zIndex={100}
-                >
-                    <Sidebar />
-                </Box>
-            ) : null}
+                <main>
+                    <Outlet />
+                </main>
+                {isLargerThanMD ? (
+                    <Box
+                        bg='background.base'
+                        position='fixed'
+                        right='0'
+                        bottom='0'
+                        top='80px'
+                        w='256px'
+                        zIndex={100}
+                    >
+                        <Sidebar />
+                    </Box>
+                ) : null}
 
-            {!isLargerThanMD && <FooterMobile />}
-        </Box>
+                {!isLargerThanMD && <FooterMobile />}
+            </Box>
+        </>
     );
 }
