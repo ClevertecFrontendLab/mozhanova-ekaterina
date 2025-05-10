6fb86a94700b750c01205b696557c3b3c9942181
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-05-10 20:40:08 +0300

    feat(user): main layout

---
+++ src/components/layouts/UserLayout.tsx
@@ -0,0 +1,92 @@
+import {
+    Box,
+    Flex,
+    Image,
+    SimpleGrid,
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
+    Text,
+    VStack,
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router';
+
+import { LogIn } from '../../pages/user/LogIn';
+import { SignIn } from '../../pages/user/SignIn';
+
+export const UserLayout = () => {
+    const navigate = useNavigate();
+
+    const handleTabChange = (index: number) => {
+        navigate(index === 0 ? '/login' : '/signin');
+    };
+
+    return (
+        <SimpleGrid
+            position='relative'
+            w='100vw'
+            h='100vh'
+            columns={{
+                base: 1,
+                md: 2,
+            }}
+        >
+            <VStack
+                bg='linear-gradient(208deg, #eaffc7 0%, #29813f 100%)'
+                align='center'
+                pt={{ base: '72px', sm: '140px', md: '170px' }}
+            >
+                <Image
+                    mb={{ base: 10, sm: 14, md: 20 }}
+                    w={{ base: '158px', md: '271px' }}
+                    src='/src/assets/logo/logo.png'
+                />
+                <Tabs
+                    w={{
+                        base: '328px',
+                        sm: '355',
+                        md: '451px',
+                        lg: '461px',
+                    }}
+                    variant='line'
+                    onChange={handleTabChange}
+                >
+                    <TabList borderBottomColor='border.light' gap={4} mb={10}>
+                        <Tab bg='transparent'>Вход на сайт</Tab>
+                        <Tab bg='transparent'>Регистрация</Tab>
+                    </TabList>
+
+                    <TabPanels>
+                        <TabPanel>
+                            <LogIn />
+                        </TabPanel>
+                        <TabPanel>
+                            <SignIn />
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </VStack>
+            <Box display={{ base: 'none', md: 'block' }}>
+                <Image objectFit='cover' h='100%' src='/src/assets/user/page_bg.png' />
+            </Box>
+            <Flex
+                justify='space-between'
+                px={5}
+                pb={{ base: 4, sm: 5 }}
+                bottom={0}
+                left={0}
+                right={0}
+                position='absolute'
+                fontSize='xs'
+                fontWeight={600}
+            >
+                <Text p={2.5}>Все права защищены, ученический файл, ©Клевер Технолоджи, 2025</Text>
+                <Text p={2.5} display={{ base: 'none', md: 'inline' }}>
+                    ̶ Лучший сервис для ваших кулинарных побед{' '}
+                </Text>
+            </Flex>
+        </SimpleGrid>
+    );
+};
