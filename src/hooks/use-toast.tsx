bb462472a2832ca6b0d6a48e531ea569de09d4a1
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-05-10 20:38:06 +0300

    refactor(toast): add duration parameter

--- src/hooks/use-toast.tsx
+++ src/hooks/use-toast.tsx
@@ -6,38 +6,16 @@ import {
     Box,
     CloseButton,
     useToast as useChakraToast,
-    UseToastOptions,
 } from '@chakra-ui/react';
 
 type ToastType = 'success' | 'error' | 'warning' | 'info';
 
 export const useToast = () => {
     const toast = useChakraToast();
-    const styles: Record<ToastType, UseToastOptions> = {
-        error: {
-            containerStyle: {
-                bg: 'error.400',
-                borderRadius: 'unset',
-                w: {
-                    base: '328px',
-                    md: '400px',
-                },
-            },
-        },
-        success: {},
-        warning: {},
-        info: {},
-    };
 
-    const showToast = (type: ToastType, title: string, description: string) =>
+    const showToast = (type: ToastType, title?: string, description?: string, duration = 3000) =>
         toast({
-            title: title,
-            description: description,
-            status: type,
-            duration: 3000,
-            isClosable: true,
-            variant: 'solid',
-            ...styles[type],
+            duration: duration,
             render: ({ onClose }) => (
                 <Alert
                     w={{ base: '328px', md: '400px' }}
@@ -59,8 +37,9 @@ export const useToast = () => {
             ),
         });
     return {
-        showError: (title: string, description: string) => showToast('error', title, description),
-        showSuccess: (title: string, description: string) =>
-            showToast('success', title, description),
+        showError: (title?: string, description?: string, duration?: number) =>
+            showToast('error', title, description, duration),
+        showSuccess: (title?: string, description?: string, duration?: number) =>
+            showToast('success', title, description, duration),
     };
 };
