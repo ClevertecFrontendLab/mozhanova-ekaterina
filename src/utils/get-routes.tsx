8301346d3838fa090318c9ab9c9a041188a3dd62
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-05-30 17:45:41 +0300

    feat(helpers): add getEditRecipePath helper

--- src/utils/get-routes.tsx
+++ src/utils/get-routes.tsx
@@ -1,6 +1,10 @@
+import { AppRoutes } from '~/constants/routes-config';
+
 export const routeHelpers = {
     getCategoryPath: (category: string) => `/${category}`,
     getSubCategoryPath: (category: string, subCategory: string) => `/${category}/${subCategory}`,
     getRecipePath: (category: string, subCategory: string, id: string) =>
         `/${category}/${subCategory}/${id}`,
+    getEditRecipePath: (category: string, subCategory: string, id: string) =>
+        `${AppRoutes.EDIT}/${category}/${subCategory}/${id}`,
 };
