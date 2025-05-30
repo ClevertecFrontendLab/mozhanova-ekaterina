46bdd83275bb6da157bdcf87b3fead4eb8e66b72
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-05-30 18:16:55 +0300

    feat(sidebar): add link to new recipe page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,26 +1,41 @@
 import { EditIcon } from '@chakra-ui/icons';
 import { Box, Flex } from '@chakra-ui/react';
+import { Link, useLocation } from 'react-router';
+
+import { AppRoutes } from '~/constants/routes-config';
+import { DATA_TEST_IDS } from '~/constants/test-ids';
 
 import { ProfileNotification } from './shared/ProfileNotification';
 import { UiIconButton } from './ui/UiIconButton';
 
-export const Sidebar = () => (
-    <Flex
-        position='relative'
-        direction='column'
-        justifyContent='space-between'
-        alignItems='center'
-        h='100%'
-        pt={4}
-    >
-        <ProfileNotification />
-
-        <Box position='absolute' bottom='52px' left={0} right={0}>
-            <UiIconButton
-                text='Записать рецепт'
-                icon={<EditIcon width='24px' height='24px' />}
-                variant='primary'
-            />
-        </Box>
-    </Flex>
-);
+export const Sidebar = () => {
+    const location = useLocation();
+    return (
+        <Flex
+            position='relative'
+            direction='column'
+            justifyContent='space-between'
+            alignItems='center'
+            h='100%'
+            pt={4}
+        >
+            {location.pathname !== AppRoutes.CREATE_RECIPE && (
+                <>
+                    <ProfileNotification />
+                    <Box position='absolute' bottom='52px' left={0} right={0}>
+                        <Link
+                            to={AppRoutes.CREATE_RECIPE}
+                            data-test-id={DATA_TEST_IDS.ADD_RECIPE_BUTTON}
+                        >
+                            <UiIconButton
+                                text='Записать рецепт'
+                                icon={<EditIcon width='24px' height='24px' />}
+                                variant='primary'
+                            />
+                        </Link>
+                    </Box>
+                </>
+            )}
+        </Flex>
+    );
+};
