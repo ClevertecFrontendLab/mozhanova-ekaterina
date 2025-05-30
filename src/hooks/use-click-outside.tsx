57cd506dc31e5bc509e87171b034c13dda6d3623
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-05-30 17:58:55 +0300

    feat(hooks): use click outside

---
+++ src/hooks/use-click-outside.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+export const useClickOutside = (trigger: boolean) => {
+    const [clickedLink, setClickedLink] = useState('');
+    useEffect(() => {
+        if (!trigger) return;
+        const handleClick = (event: MouseEvent) => {
+            const link = (event.target as Element)?.closest('a');
+            if (link) {
+                event.preventDefault();
+                event.stopPropagation();
+                event.stopImmediatePropagation();
+
+                setClickedLink(link.pathname);
+            }
+        };
+
+        document.addEventListener('click', handleClick, true);
+
+        return () => {
+            document.removeEventListener('click', handleClick, true);
+        };
+    }, [trigger]);
+
+    return { clickedLink };
+};
