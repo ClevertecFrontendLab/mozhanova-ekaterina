6bcad950e5818fa98d152922a1e8534bc3bf5431
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-05-30 18:28:32 +0300

    feat(logo): add test id

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router';
 
 import logo from '~/assets/logo/logo.png';
 import logo_mobile from '~/assets/logo/mobile_logo.png';
+import { AppRoutes } from '~/constants/routes-config';
+import { DATA_TEST_IDS } from '~/constants/test-ids';
 import { useBreakpoint } from '~/hooks/use-breakpoint';
 
 export const Logo = () => {
     const [isLargerThanSM] = useBreakpoint('sm');
 
     return (
-        <Box width='256px'>
-            <Link to='/'>
+        <Box width='256px' data-test-id={DATA_TEST_IDS.HEADER_LOGO}>
+            <Link to={AppRoutes.HOME}>
                 <Image
                     w={{
                         base: '32px',
