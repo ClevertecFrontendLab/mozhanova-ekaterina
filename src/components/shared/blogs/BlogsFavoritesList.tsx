ac894612867f6dfaeb80ea11f4992d218b8b1a38
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-06-04 00:05:32 +0300

    feat(blogs): create favorites list

---
+++ src/components/shared/blogs/BlogsFavoritesList.tsx
@@ -0,0 +1,99 @@
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+import { Flex, Heading, SimpleGrid } from '@chakra-ui/react';
+import { Link } from 'react-router';
+
+import avatar_1 from '~/assets/blog_avatar_1.png';
+import { UiButton } from '~/components/ui/UiButton';
+import { AppRoutes } from '~/constants/routes-config';
+import { useBreakpoint } from '~/hooks/use-breakpoint';
+import { Blogger } from '~/types';
+
+import { BlogCard } from './BlogCard';
+
+export const BlogsFavoritesList = ({
+    bloggers,
+    heading,
+}: {
+    bloggers: Blogger[] | undefined;
+    heading?: string;
+}) => {
+    const [isLargerThanMD] = useBreakpoint('md');
+
+    if (!bloggers) return null;
+    return (
+        <Flex
+            direction='column'
+            bg='primary.200'
+            p={{
+                base: 3,
+                md: 6,
+            }}
+            borderRadius='16px'
+            gap={{
+                base: 3,
+                md: 4,
+            }}
+        >
+            <Flex justifyContent='space-between'>
+                <Heading
+                    as='h2'
+                    fontSize={{
+                        base: '24px',
+                        md: '36px',
+                    }}
+                    fontWeight='500'
+                >
+                    {heading}
+                </Heading>
+                {isLargerThanMD && (
+                    <Link to={AppRoutes.BLOGS}>
+                        <UiButton
+                            text='Все авторы'
+                            variant='primaryGhost'
+                            rightIcon={<ArrowForwardIcon />}
+                            size='lg'
+                        />
+                    </Link>
+                )}
+            </Flex>
+            <SimpleGrid
+                columns={{
+                    base: 1,
+                    sm: 2,
+                    md: bloggers.length > 8 ? 3 : 2,
+                }}
+                gap={{
+                    base: 3,
+                    md: 4,
+                }}
+            >
+                {bloggers.map((blogger) => (
+                    <BlogCard
+                        avatarSrc={avatar_1}
+                        key={blogger._id}
+                        name={[blogger.firstName, blogger.lastName]}
+                        note={blogger.notes[0]?.text}
+                        login={blogger.login}
+                        newRecipesCount={blogger.newRecipesCount}
+                        bloggerId={blogger._id}
+                        bookmarksCount={blogger.bookmarksCount}
+                        subscribersCount={blogger.subscribersCount}
+                        showControls
+                        showStats
+                        isFavorite
+                    />
+                ))}
+            </SimpleGrid>
+            {!isLargerThanMD && (
+                <Link to={AppRoutes.BLOGS}>
+                    <UiButton
+                        text='Все авторы'
+                        variant='primaryGhost'
+                        rightIcon={<ArrowForwardIcon />}
+                        size='md'
+                    />
+                </Link>
+            )}
+        </Flex>
+    );
+};
