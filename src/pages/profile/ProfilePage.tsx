69cb695bc3ea81f641641a66e0c8bdd2bea0d1a2
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-06-22 17:34:25 +0300

    refactor(profile page): replace useGetProfileQuery to selector

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -6,10 +6,11 @@ import { Hero } from '~/components/shared/profile/Hero';
 import { NotesList } from '~/components/shared/profile/NotesList';
 import { RecipesList } from '~/components/shared/profile/RecipesList';
 import { useLazyGetRecipesByUserIdQuery } from '~/query/recipe-api';
-import { useGetProfileQuery } from '~/query/user-api';
+import { useAppSelector } from '~/store/hooks';
+import { selectCurrentUser } from '~/store/user-slice';
 
 export const ProfilePage = () => {
-    const { data: profile } = useGetProfileQuery();
+    const profile = useAppSelector(selectCurrentUser);
     const [getRecipes, { data: recipes }] = useLazyGetRecipesByUserIdQuery();
 
     useEffect(() => {
@@ -21,10 +22,10 @@ export const ProfilePage = () => {
     return (
         <Grid as='main' px={{ base: 4, sm: 5, md: 6 }}>
             <Hero
-                firstName={profile?.firstName}
-                lastName={profile?.lastName}
-                login={profile?.login}
-                avatar={profile?.photoLink}
+                firstName={profile.firstName}
+                lastName={profile.lastName}
+                login={profile.login}
+                avatar={profile.photoLink}
             />
             <Grid gap={{ base: 8, md: 10 }}>
                 <RecipesList drafts={profile.drafts} recipes={recipes.recipes} />
