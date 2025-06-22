705c3e166ef3f47f8f228520490c037514d9fa2b
Ekaterina Mozhanova <pandafomka@gmail.com>
2025-06-22 17:28:05 +0300

    feat(user slice): add all users state, selector and action

--- src/store/user-slice.ts
+++ src/store/user-slice.ts
@@ -1,23 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-import { StatisticDto, UserDto } from '~/types';
+import { ProfileDto, StatisticDto, UserDto } from '~/types';
 
 import { ApplicationState } from './configure-store';
 
 export type UserState = {
-    currentUser: UserDto | null;
+    currentUser: ProfileDto | null;
     accessToken: string | null;
     statistics: StatisticDto | null;
+    allUsers: UserDto[];
 };
 
 export const selectCurrentUser = (state: ApplicationState) => state.user.currentUser || null;
 export const accessToken = (state: ApplicationState) => state.user.accessToken || null;
 export const selectStatistics = (state: ApplicationState) => state.user.statistics || null;
+export const selectAllUsers = (state: ApplicationState) => state.user.allUsers || [];
 
 const initialState: UserState = {
     currentUser: null,
     accessToken: null,
     statistics: null,
+    allUsers: [],
 };
 
 export const userSlice = createSlice({
@@ -33,7 +36,10 @@ export const userSlice = createSlice({
         setStatistics: (state, { payload }: { payload: StatisticDto | null }) => {
             state.statistics = payload;
         },
+        setAllUsers: (state, { payload }: { payload: UserDto[] }) => {
+            state.allUsers = payload;
+        },
     },
 });
 
-export const { setUser, setCredentials, setStatistics } = userSlice.actions;
+export const { setUser, setCredentials, setStatistics, setAllUsers } = userSlice.actions;
